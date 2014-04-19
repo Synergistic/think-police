3690b5c72810dc68f4ddba70bc0be41922419a32
Synergistic <DFernandez226@gmail.com>
2014-04-19 12:47:09 -0400

    New game transition
    Transition screen between new game and game screen added. Removed
    settings/pause

--- menus.py
+++ menus.py
@@ -2,6 +2,7 @@ __author__ = 'Synergy'
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from transition import change_to_transition
 
 Builder.load_string('''<Main>:
     name: 'main'
@@ -15,34 +16,14 @@ Builder.load_string('''<Main>:
             on_press: root.new_game()
         Button:
             text: 'Settings'
-            on_press: root.manager.current = 'settings'
+            on_press: app.open_settings()
 ''')
 
 
 class Main(Screen):
     
     def new_game(self):
-        self.manager.current = 'desk'
+        change_to_transition(self.manager, text='Day 1')
         self.manager.parent.playing = True
         self.manager.parent.current_day = 1
-        print self.width, self.height
         
-    
-Builder.load_string('''<Settings>:
-    name: 'settings'
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Settings'
-            font_size: '60sp'
-        Button:
-            text: 'Back'
-            on_press: root.manager.current = 'main'
-''')
-    
-    
-class Settings(Screen):
-    pass
-
-class Pause(Screen):
-    pass
