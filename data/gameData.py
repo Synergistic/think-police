f6a9bbe28b83f0ee62dc46f542d67451daf6eb9d
Synergistic <DFernandez226@gmail.com>
2014-04-22 13:10:53 -0400

    Fixed Syntax Errors
    Fixed some syntax errors in the crimes dict

--- data/gameData.py
+++ data/gameData.py
@@ -3,18 +3,19 @@ __author__ = 'Synergy'
 RULES = ['Inner Members are not to be killed nor reeducated.',
          'Prominent members are not to be reeducated.',
          'Parole are not to be killed.',
-         'Repeat offenders should be reeducated at a minimum']
+         'Repeat offenders should be reeducated at a minimum',
+         'I need to make up a fifth rule']
 
 CRIMES = {'Treason': ['The crime of betraying The Party',
                       'Reeducation', 
                       'Death'],
-          'ThoughtCrime': ['The criminal act of holding unspoken beliefs or doubts that oppose or question The Party',
+          'Thoughtcrime': ['The criminal act of holding unspoken beliefs\n or doubts that oppose or question The Party',
                             'Death',
                             'Death'],
-          'Violence': ['The behavior involving physical force intended to hurt, damage, or kill a fellow comrade.',
-                        'Imprisonment'
+          'Violence': ['The behavior involving physical force intended to\n hurt, damage, or kill a fellow comrade.',
+                        'Imprisonment',
                         'Reeducation'],
-          'Facecrime': ['To wear an improper expression on your face, i.e. to grimace at a victory announcement',
+          'Facecrime': ['To wear an improper expression on your face,\n i.e. to grimace at a victory announcement',
                          'Reeducation',
                          'Death'],
           'Sexcrime': ['The act of sex for any reason other than procreation.',
